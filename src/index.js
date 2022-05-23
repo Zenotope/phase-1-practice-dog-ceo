e3c01d308835511b51c9ab3d4ed9ffdc4301c422
zaak <zaak.kerstetter@gmail.com>
2022-05-23 17:01:25 -0600

    commit tree 64ed6be8ab80347555bf82f8fe0079dee1b85cf1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 console.log('%c HI', 'color: firebrick')
+
+
+// document.addEventListener('DOMContentLoad', loadDogImg){
+//     .then(function(loadDogImg){
+//         return response.json();
+//     })
+//     .then(function(data)){
+
+//     })
+// }
+// const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(function(response){
+        return response.json(); 
+    })
+    .then(function(data){
+
+    })
